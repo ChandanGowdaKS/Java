4bb050f9d83750c1045e698dacc90d9e719fd501
Chandan Gowda K S <chanduchess122@gmail.com>
2025-03-17 23:11:45 +0530

    Add files via upload

---
+++ Java basic/main.java
@@ -0,0 +1,11 @@
+
+
+public class main{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        boolean isjava = true;
+    boolean isfun = false;
+    System.out.println(isjava);
+    System.out.println(isfun);
+}
+}
