387dea9ab362e034f88b5fd2bf826a0ecdceaa79
Chandan Gowda K S <chanduchess122@gmail.com>
2025-03-23 00:27:49 +0530

    Add files via upload

---
+++ Java basic/Ifelse.java
@@ -0,0 +1,35 @@
+
+
+import java.util.Scanner;
+
+
+public class Ifelse {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Enter a number");
+        if (scanner.hasNextInt()) {
+            
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if (n % 2 == 0 && n >=2 && n<=5 || n % 2 == 0 && n > 20){
+            System.out.println("Not Weird");
+        }
+        else if(n % 2== 0 && n >=6 && n <= 20){
+            System.out.println("Weird");
+        }
+        else if(n % 2!=0){
+            System.out.println("Weird");
+        }
+    }
+    else{
+        System.out.println("Please enter a valid number");
+    }
+
+        scanner.close();
+    }
+}
+ 
--- Java basic/Solution.java
+++ Java basic/Solution.java
@@ -53,4 +53,3 @@ public class Solution {
         scanner.close();
     }
 }
-// complted simple hackerrank problem
