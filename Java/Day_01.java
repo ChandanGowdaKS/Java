d802caa595d309f46ef02771ea7dfb42f577c8f5
Chandan Gowda K S <chanduchess122@gmail.com>
2025-04-05 21:02:39 +0530

    Add files via upload

---
+++ Java/Day_01.java
@@ -0,0 +1,15 @@
+import java.util.*;
+//import java.util.Scanner;
+//package java;
+
+public class Day_01 {
+
+    public static void main(String[] args){
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Hello CHnadu");
+        System.out.println("Enter your age");
+        int age = inp.nextInt();
+        System.out.println("The age is :"+ age);
+
+    }
+}
