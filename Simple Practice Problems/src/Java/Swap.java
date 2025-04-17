9f827f82e98b6e09da0111300c3b51c7fa35cbbe
Chandan Gowda K S <chanduchess122@gmail.com>
2025-04-17 23:25:09 +0530

    Add files via upload

---
+++ Simple Practice Problems/src/Java/Swap.java
@@ -0,0 +1,25 @@
+// Swapping Two numbers
+package Java;
+
+public class Swap {
+    public static void main(String[] args){
+        // using temporary variable
+        int a = 10;
+        int b = 20;
+        System.out.println(" Before Swapping a = "+ a + " b = "+ b);
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println(" After Swapping a = "+ a + " b = "+ b);
+
+        // without temporary variable
+        a = 10;
+        b = 20;
+        a = a+b;
+        b = a - b;
+        a = a - b;
+        System.out.println("without temporary variable After Swapping  a = "+ a + " b = "+ b);
+
+
+    }
+}
